a830de23d88f89e995e1af6a051f8eb8da59eac6
YoonHan0 <dbsgksdud12@naver.com>
2023-02-23 14:38:01 +0900

    global, window

---
+++ javascript-practice/basics/ex18.js
@@ -0,0 +1,13 @@
+/*
+    전역 객체(Global Object)
+
+    node: global
+    browser: window
+
+*/
+
+global.n = '윤한영';
+console.log(global.n, n);
+
+var email = 'yoonHan0@gmail.com';
+console.log(global.email, email);
