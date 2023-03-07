1215efefb029718dba5024e62312e7555338085f
YoonHan0 <dbsgksdud12@naver.com>
2023-03-07 09:13:00 +0900

    주석 추가

--- javascript-practice/async/ex02.js
+++ javascript-practice/async/ex02.js
@@ -1,5 +1,6 @@
 
 /* 콜백 지옥에서 벗어나기 위해서 promise 라는 것을 사용하게 됨 */
+// callback 대신에 promise 객체를 리턴함 -> ?: ES6에 나온 async, await를 사용하기 위해서
 const myAsyncFn02 = function(param) {
     return new Promise(function(resolve, reject) {
         /*
@@ -12,12 +13,12 @@ const myAsyncFn02 = function(param) {
             } else {
                 reject(new Error('fail'));
             }
-            
            }, 2000);
     });
 }
 
 // test01: success  |   비동기 처리를 할 때 callback함수는 첫 인자를 error, 두번째 인자는 result를 받는 것이 일반적임
+/*
 myAsyncFn02("data").then(function(error, result) {
     console.log(result);
 })
@@ -26,7 +27,9 @@ myAsyncFn02("data").then(function(error, result) {
 myAsyncFn02("").catch(function(error) {
     console.error(error);
 });
-/* 이렇게 한 번에 가능
+*/
+
+/* 이렇게 한 번에 가능 */
 myAsyncFn02("data")
     .then(function(error, result) {
         console.log(result);
@@ -34,6 +37,6 @@ myAsyncFn02("data")
     .catch(function(error) {
         console.error(error);
     });
-*/
+
 
 console.log("wait...");
